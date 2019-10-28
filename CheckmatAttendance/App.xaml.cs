da48fe54c49887e00758e25d31e71a57683397d8
ЖуртановЭ <@L206-4>
2019-10-28 12:54:46 +0600

    Добавьте файлы проекта.

---
+++ CheckmatAttendance/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Egor92.UINavigation.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CheckmatAttendance
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var window = new MainWindow();
+            var navigationManager = new NavigationManager(window);
+
+            /*
+            navigationManager.Register<FirstView>("FirstKey", () => new FirstViewModel(navigationManager));
+            navigationManager.Register<SecondView>("SecondKey", () => new SecondViewModel(navigationManager));
+            */
+
+            navigationManager.Navigate("FirstKey");
+            window.Show();
+        }
+    }
+}
